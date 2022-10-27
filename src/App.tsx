48f53fc060c57849135213d591f2d4afa731d49c
Kwon Valerii <wellko24@gmail.com>
2022-10-27 20:50:16 +0600

    task 1 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Task from "./lib/Task";
+import AddTaskForm from "./lib/AddTaskForm";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [task, setTask] = useState([
+		{text: 'Wake up', id: '99'},
+		{text: 'Go to the bathroom', id: '100'}
+	])
+
+	const AddTask = (event: React.ChangeEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const newBlock = {...task[0]};
+		newBlock.text = event.target.input.value;
+		const newBlockCopy = [...task];
+		newBlockCopy.push(newBlock);
+		setTask(newBlockCopy);
+		event.target.input.value = '';
+	}
+
+	const deleteTask = (index: number) => {
+		const taskCopy = [...task];
+		taskCopy.splice(index, 1);
+		setTask(taskCopy);
+	}
+
+	let Tasks = (
+		<div>
+			{task.map((task, i) => (
+				<Task
+					key={i}
+					text={task.text}
+					id={i.toString()}
+					delete={() => deleteTask(i)}
+				/>
+			))}
+		</div>
+	);
+
+	return (
+		<div className="App">
+			<AddTaskForm onSubmit={AddTask}/>
+			{Tasks}
+		</div>
+	);
 }
 
 export default App;
