ae5289298a6cf037876373ba6b3e378eec35273d
Kwon Valerii <wellko24@gmail.com>
2022-10-28 01:27:07 +0600

    makes styles for App

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,20 @@ import AddTaskForm from "./lib/AddTaskForm";
 
 
 function App() {
+
+	let id = 1;
+
 	const [task, setTask] = useState([
-		{text: 'Wake up', id: '0', done: ''},
-		{text: 'Go to the bathroom', id: '1', done: ''}
+		{text: 'Wake up', id: '0', done: 'flexBox'},
+		{text: 'Go to the bathroom', id: '1', done: 'flexBox'}
 	])
 
 	const AddTask = (event: React.ChangeEvent<HTMLFormElement>) => {
 		event.preventDefault();
+		id++
 		const newBlock = {...task[0]};
 		newBlock.text = event.target.input.value;
+		newBlock.id = id.toString();
 		const newBlockCopy = [...task];
 		newBlockCopy.push(newBlock);
 		setTask(newBlockCopy);
@@ -40,7 +45,7 @@ function App() {
 		<div>
 			{task.map((task, i) => (
 				<Task
-					done={checked[i] ? 'done' : ''}
+					done={checked[i] ? 'flexBox done' : 'flexBox'}
 					key={i}
 					text={task.text}
 					delete={() => deleteTask(i)}
--- src/lib/Task.tsx
+++ src/lib/Task.tsx
@@ -5,9 +5,11 @@ const Task: React.FC<TaskProps> = props => {
 	return (
 		<div className='toDo'>
 			<div className={props.done}>
-			<span>{props.text}</span>
-			<input type='checkbox' onChange={props.change}/>
-			<img src={trash} onClick={props.delete} alt='Trash'/>
+				<span>{props.text}</span>
+				<div>
+					<input type='checkbox' onChange={props.change}/>
+					<img src={trash} onClick={props.delete} alt='Trash'/>
+				</div>
 			</div>
 		</div>
 	)
@@ -18,8 +20,8 @@ interface TaskProps extends React.PropsWithChildren {
 	text: string;
 	id?: string;
 	done: string;
-	delete?: React.MouseEventHandler;
-	change?: React.ChangeEventHandler<HTMLInputElement>;
+	delete: React.MouseEventHandler;
+	change: React.ChangeEventHandler<HTMLInputElement>;
 }
 
 export default Task;
